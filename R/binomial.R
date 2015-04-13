28119c51321fe06b2aa7c30db19505b7e56fe0fe
Narayanan Raghupathy <narayananr@users.noreply.github.com>
2015-04-12 23:26:55 -0400

    Update binomial.R

--- R/binomial.R
+++ R/binomial.R
@@ -16,7 +16,13 @@
 #' @examples
 #' R code here showing how your function works
 
-nodSum.np= apply(nodCnt.np,1,sum)
-totSum.np = apply(totCnt.np,1,sum)
-bpvals.np= apply(cbind(nodSum.np,totSum.np),1,
-                 function(x){binom.test(round(x[1]),round(x[2]),alternative="two.sided")$p.value})
+single.sample.binomial <- function(data_mat){
+     pvals = apply(data_mat,1,
+             function(x){binom.test(round(x[1]),round(x[2]),alternative="two.sided")$p.value})
+     return(pvals
+}
+
+#nodSum.np= apply(nodCnt.np,1,sum)
+#totSum.np = apply(totCnt.np,1,sum)
+#bpvals.np= apply(cbind(nodSum.np,totSum.np),1,
+#                 function(x){binom.test(round(x[1]),round(x[2]),alternative="two.sided")$p.value})
